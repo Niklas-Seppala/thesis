26da4441b566e497ea59f13f25057200e05b4aa7
Niklas Seppala <niklas.t.seppala@gmail.com>
2023-10-18 16:58:28 +0300

    wip

---
+++ src/test/java/org/nse/thesis/wordindex/DirectoryWordIndex.java
@@ -0,0 +1,86 @@
+package org.nse.thesis.wordindex;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class DirectoryWordIndex implements AutoCloseable {
+
+    private final Map<String, WordIndex> indexMap;
+
+    public DirectoryWordIndex(String dirPath, WordIndex.IndexProvider indexProvider) throws FileNotFoundException {
+
+        final Path path = Path.of(dirPath);
+
+        if (Files.notExists(path)) {
+            throw new FileNotFoundException(dirPath);
+        }
+        if (Files.isDirectory(path)) {
+            throw new FileNotFoundException(
+                    String.format("%s is not a directory", dirPath));
+        }
+        File dir = path.toFile();
+        assert dir.canRead();
+
+        this.indexMap = new HashMap<>();
+        File[] files = dir.listFiles();
+        if (files != null) {
+            indexFilesInDirectory(Arrays.asList(files), indexProvider);
+        }
+    }
+
+    private void indexFilesInDirectory(List<File> files,
+                                       WordIndex.IndexProvider indexProvider) {
+        if (files.isEmpty()) {
+            return;
+        }
+        for (File file : files) {
+            String fileName = file.getName();
+            if (!indexMap.containsKey(fileName)) {
+                try {
+                    WordIndex index =
+                            indexProvider.indexFrom(file.getAbsolutePath());
+                    this.indexMap.put(fileName, index);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public Collection<String> getWordsWithContextInFile(String path, String word,
+                                                        WordIndex.ContextBytes ctx) {
+        String fileName = Path.of(path).getFileName().toString();
+        WordIndex index = this.indexMap.get(fileName);
+        if (index != null) {
+            return index.getWords(word, ctx);
+        } else {
+            return null;
+        }
+    }
+
+    public Iterator<String> iterateWordsWithContextInFile(String path, String word,
+                                                          WordIndex.ContextBytes ctx)
+            throws FileNotFoundException {
+        String fileName = Path.of(path).getFileName().toString();
+        WordIndex index = this.indexMap.get(fileName);
+        if (index != null) {
+            return index.iterateWords(word, ctx);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void close() {
+        this.indexMap.values().forEach(index -> {
+            try {
+                index.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
---
+++ src/test/java/org/nse/thesis/wordindex/DirectoryWordIndexTest.java
@@ -0,0 +1,59 @@
+package org.nse.thesis.wordindex;
+
+import org.junit.jupiter.api.Test;
+import org.nse.thesis.wordindex.ffm.FFMWordIndex;
+import org.nse.thesis.wordindex.jna.JNAWordIndex;
+import org.nse.thesis.wordindex.jni.JNIWordIndex;
+import org.nse.thesis.wordindex.pojo.ImprovedJavaWordIndex;
+import org.nse.thesis.wordindex.pojo.JavaWordIndex;
+
+import java.io.FileNotFoundException;
+
+class DirectoryWordIndexTest {
+
+    @Test
+    void asd() throws FileNotFoundException {
+        final String DIR = "dir";
+        final String WORD = "god";
+        final String FILE = "bible.txt";
+
+        // Open indexes
+
+        DirectoryWordIndex index = new DirectoryWordIndex(DIR, JavaWordIndex::new);
+
+        DirectoryWordIndex indexerImproved = new DirectoryWordIndex(DIR,
+                ImprovedJavaWordIndex::new);
+
+        DirectoryWordIndex indexJNI = new DirectoryWordIndex(DIR,
+                (p) -> new JNIWordIndex(p, 1 << 8,
+                        8192, 4096, true));
+
+
+        DirectoryWordIndex indexJNA = new DirectoryWordIndex(DIR,
+                (p) -> new JNAWordIndex(p, 1 << 8,
+                        8192, 4096, true));
+
+
+        DirectoryWordIndex indexFFM = new DirectoryWordIndex(DIR,
+                (p) -> new FFMWordIndex(p, 1 << 8,
+                        8192, 4096, true));
+
+        // Query
+
+        index.getWordsWithContextInFile(FILE, WORD,
+                WordIndex.ContextBytes.SMALL_CONTEXT);
+        index.close();
+
+        indexerImproved.getWordsWithContextInFile(FILE, WORD, WordIndex.ContextBytes.SMALL_CONTEXT);
+        indexerImproved.close();
+
+        indexJNA.getWordsWithContextInFile(FILE, WORD, WordIndex.ContextBytes.SMALL_CONTEXT);
+        indexJNA.close();
+
+        indexJNI.getWordsWithContextInFile(FILE, WORD, WordIndex.ContextBytes.SMALL_CONTEXT);
+        indexJNI.close();
+
+        indexFFM.getWordsWithContextInFile(FILE, WORD, WordIndex.ContextBytes.SMALL_CONTEXT);
+        indexFFM.close();
+    }
+}
